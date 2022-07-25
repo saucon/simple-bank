13a99dafc2fd96723fd533f35d225cc30a2cf03e
saucon <saucon.mfh@gmail.com>
2022-07-26 00:07:43 +0700

    add router

--- customer/cmd/customerapp/main.go
+++ customer/cmd/customerapp/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "log"
+import (
+	"github.com/Saucon/simple-bank/account/pkg/env"
+	"github.com/Saucon/simple-bank/customer/configs/ginconf"
+	"log"
+)
 
 func main() {
-	log.Println("this is customer app.....")
+	// get env
+	env.NewEnv("customer/.env")
+	cfg := env.Config
+
+	router := ginconf.NewRouter()
+
+	if err := router.Run(cfg.Host + ":" + cfg.Port); err != nil {
+		log.Fatalf("Something was wrong with "+cfg.Host+":"+cfg.Port, err)
+	}
 }
